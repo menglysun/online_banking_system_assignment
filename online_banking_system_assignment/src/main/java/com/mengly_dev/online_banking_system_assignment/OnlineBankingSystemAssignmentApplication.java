6ab08d01b8b8ef812a2a8f0ec0854ce071077dd4
Sun Mengly <147031669+menglysun@users.noreply.github.com>
2025-09-09 11:08:44 +0700

    Add files via upload
    first initial.

---
+++ online_banking_system_assignment/src/main/java/com/mengly_dev/online_banking_system_assignment/OnlineBankingSystemAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.mengly_dev.online_banking_system_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBankingSystemAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineBankingSystemAssignmentApplication.class, args);
+    }
+
+}
---
+++ online_banking_system_assignment/src/test/java/com/mengly_dev/online_banking_system_assignment/OnlineBankingSystemAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mengly_dev.online_banking_system_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBankingSystemAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
